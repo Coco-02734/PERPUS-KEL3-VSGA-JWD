052159a80462fcaef39a58a548c236ff853b579f
Coco-02734 <68150655+Coco-02734@users.noreply.github.com>
2021-08-22 23:26:08 +0700

    Update Cetak Pengembalian - Widi

--- pages/cetak_kartu_pengembalian.php
+++ pages/cetak_kartu_pengembalian.php
@@ -1,34 +1,48 @@
 <?php
 	include "../koneksi.php";
-	$id_transaksi=$_GET['id'];
-	$q_tampil_pengembalian=mysqli_query($db,"SELECT tr.idtransaksi, tag.nama, tb.judulbuku, tr.tglpinjam, tr.tglkembali FROM tbtransaksi tr INNER JOIN tbanggota tag ON tr.idanggota = tag.idanggota INNER JOIN tbbuku tb ON tr.idbuku = tb.idbuku WHERE tr.idtransaksi='$id_transaksi'");
-	$r_tampil_pengembalian=mysqli_fetch_array($q_tampil_pengembalian);
+	date_default_timezone_set('Asia/Jakarta');
+	require_once("../assets/dompdf/autoload.inc.php");
+
+	use Dompdf\Dompdf;
+
+	$dompdf = new Dompdf();
+	$html = '<center><h3>Data Kartu Pengembalian</h3></center><br/>';
+	$html .= '<table border="1" width="100%">
+	 <tr>
+	 
+	 <th>ID Transaksi</th>
+	 <th>Nama</th>
+	 <th>Judul Buku</th>
+	 <th>Tanggal Pengembalian</th>
+	 </tr>';
+
+	 $query = "SELECT tr.idtransaksi, tag.nama, tb.judulbuku, tr.tglkembali FROM tbtransaksi tr INNER JOIN tbanggota tag ON tr.idanggota = tag.idanggota INNER JOIN tbbuku tb ON tr.idbuku = tb.idbuku ORDER BY tr.idtransaksi ASC";
+	 $q_tampil_pengembalian = mysqli_query($db, $query);
+	 if (mysqli_num_rows($q_tampil_pengembalian) > 0) {
+			 while ($r_tampil_pengembalian = mysqli_fetch_array($q_tampil_pengembalian)) {
+					 $html .= "<tr>
+
+		<td>" . $r_tampil_pengembalian['idtransaksi'] . "</td>
+		<td>" . $r_tampil_pengembalian['nama'] . "</td>
+		<td>" . $r_tampil_pengembalian['judulbuku'] . "</td>
+		<td>" . $r_tampil_pengembalian['tglkembali'] . "</td>
+	 
+		</tr>";
+			 }
+	 }
+	 $html .= "</html>";
+	// var_dump($r_tampil_pengembalian);
+	//  die();
+	 
+	 //download pdf
+	 $dompdf->loadHtml($html);
+	 $dompdf->setPaper('A4', 'landscape');
+	 $dompdf->render();
+	 $pdf = $dompdf->output();
+	 $time = date("Y-m-d h:i:sa");
+	 $dompdf->stream($time . '_laporan.pdf', array('Attachment' => 0));
+
+
+
 ?>
-<div id="label-page"><h3>Kartu Pengembalian</h3></div>
-<div id="content">
-	<table id="tabel-input">
-		<tr>
-			<td class="label-formulir">ID Transaksi</td>
-			<td>:</td>
-			<td class="isian-formulir"><?php echo $r_tampil_pengembalian['idtransaksi']; ?></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">Nama</td>
-			<td>:</td>
-			<td class="isian-formulir"><?php echo $r_tampil_pengembalian['nama']; ?></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">Judul Buku</td>
-			<td>:</td>
-			<td class="isian-formulir"><?php echo $r_tampil_pengembalian['judulbuku']; ?></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">Tanggal Pengembalian</td>
-			<td>:</td>
-			<td class="isian-formulir"><?php echo $r_tampil_pengembalian['tglkembali']; ?></td>
-		</tr>
-	</table>
-</div>
-<script>
-		window.print();
-	</script>
+
