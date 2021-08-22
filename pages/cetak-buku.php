c73bd1e738eb4b00b0e2b10b9f5c38a184efe01b
Coco-02734 <68150655+Coco-02734@users.noreply.github.com>
2021-08-22 23:43:20 +0700

    Revisi-Cetak-Buku-Devi

--- pages/cetak-buku.php
+++ pages/cetak-buku.php
@@ -6,27 +6,32 @@ include "../koneksi.php";
 use Dompdf\Dompdf;
 
 $dompdf = new Dompdf();
+
 $html = '<center><h3>Cetak Buku</h3></center><br/>';
 $html .= '<table border="1" width="100%">
  <tr>
  <th>No</th>
  <th>ID Buku</th>
+ <th>Judul Buku</th>
  <th>Kategori</th>
  <th>Pengarang</th>
  <th>Penerbit</th>
  </tr>';
-
- $html .= "<tr>
-
- <td>" . $r_tampil_buku['no'] . "</td>
- <td>" . $r_tampil_buku['idbuku'] . "</td>
- <td>" . $r_tampil_buku['kategori'] . "</td>
- <td>" . $r_tampil_buku['pengarang'] . "</td>
- <td>" . $r_tampil_buku['penerbit'] . "</td>
-
- </tr>";
-
- $html .= "</html>";
+$nomor = 1;
+$query = "SELECT * FROM tbbuku";
+$q_tampil_buku = mysqli_query($db, $query);
+while ($r_tampil_buku = mysqli_fetch_array($q_tampil_buku)) {
+    $html .= '<tr>
+ <td>' . $nomor . '</td>
+ <td>' . $r_tampil_buku['idbuku'] . '</td>
+ <td>' . $r_tampil_buku['judulbuku'] . '</td>
+ <td>' . $r_tampil_buku['kategori'] . '</td>
+ <td>' . $r_tampil_buku['pengarang'] . '</td>
+ <td>' . $r_tampil_buku['penerbit'] . '</td>
+ </tr>';
+    $nomor++;
+}
+$html .= '</html>';
 
 //download pdf
 $dompdf->loadHtml($html);
@@ -35,45 +40,3 @@ $dompdf->render();
 $pdf = $dompdf->output();
 $time = date("Y-m-d h:i:sa");
 $dompdf->stream($time . '_laporan.pdf', array('Attachment' => 0));
-
-
-?>
-<link rel="stylesheet" type="text/css" href="../style.css">
-<h3>Data Buku</h3>
-</div>
-<div id="content">
-    <table border="1" id="tabel-tampil">
-        <tr>
-            <th id="label-tampil-no">No</th>
-            <th>ID Buku</th>
-            <th>Judul Buku</th>
-            <th>Katagori</th>
-            <th>Pengarang</th>
-            <th>Penerbit</th>
-        </tr>
-
-        <?php
-        $nomor = 1;
-        $query = "SELECT * FROM tbbuku";
-        $q_tampil_buku = mysqli_query($db, $query);
-        if (mysqli_num_rows($q_tampil_buku) > 0) {
-            while ($r_tampil_buku = mysqli_fetch_array($q_tampil_buku)) {
-        ?>
-                <tr>
-                    <td><?php echo $nomor; ?></td>
-                    <td><?php echo $r_tampil_buku['idbuku']; ?></td>
-                    <td><?php echo $r_tampil_buku['judulbuku']; ?></td>
-                    <td><?php echo $r_tampil_buku['kategori']; ?></td>
-                    <td><?php echo $r_tampil_buku['pengarang']; ?></td>
-                    <td><?php echo $r_tampil_buku['penerbit']; ?></td>
-                </tr>
-        <?php $nomor++;
-            }
-        } else {
-            echo "<tr><td colspan=6>Data Tidak Ditemukan</td></tr>";
-        } ?>
-    </table>
-    <script>
-        window.print();
-    </script>
-</div>
